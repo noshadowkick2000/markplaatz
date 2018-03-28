e9f5dca9f5b7517004c79ab2f90e95b60c079ce6
Azuragasetsu <shiranohajime@gmail.com>
2018-03-28 09:59:46 +0200

    fff
    fff

--- login_auth.php
+++ login_auth.php
@@ -15,17 +15,27 @@ $conn = mysqli_connect('azura.ga', $username, $password, $dbname);
 
 //werkelijke password opvragen
 $sql_pwGet = "SELECT Wachtwoord FROM Akkount WHERE Username='$us'";
-$pw_real = mysql_fetch_array($conn->query($sql_pwGet));
+$result = $conn->query($sql_pwGet);
+$pw_real = "";
+
+if ($result->num_rows > 0){
+	while($row = $result->fetch_assoc()){
+		$pw_real = $row['Wachtwoord'];
+	}
+}
+
+
+
+
 
 echo $pw_real;
 if ($pw == $pw_real){
 	session_start();
 	$_SESSION['STAGE'] = 1;
 	$_SESSION['USRN'] = $us;
-	haeder("Location: index.php");
+	header("Location: index.php");
 }else{
 	echo "Login Failed.";
-	
 	echo $pw;
 }
 
--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function cLoad(){
-	alert('test');
 	alert(stage);
 	switch(stage){
 		case 0:
-			document.getElementById('nHEAD').innerHTML = "U bent nog niet ingelogd.";
+			alert("U bent niet ingelogd.");
+			/*document.getElementById('nHEAD').innerHTML = "U bent nog niet ingelogd.";*/
 			break;
 		;
 		case 1:
-			document.getElementById('nHEAD').innerHTML = "Welkom, " + usrn;
+			alert("U bent ingelogd. Welkom, " + usrn);
+			/*document.getElementById('nHEAD').innerHTML = "Welkom, " + usrn;*/
 			break;
 		;
 		case 2:
-			document.getElementById('nHEAD').innerHTML = "Registreren geslaagd.";
+			alert("Registreren geslaagd.");
+			/*document.getElementById('nHEAD').innerHTML = "Registreren geslaagd.";*/
 			break;
 		;
 		case 3:
-			document.getElementById('nHEAD').innerHTML = "Registreren mislukt.";
+			alert("Registreren mislukt.");
+			/*document.getElementById('nHEAD').innerHTML = "Registreren mislukt.";*/
 			break;
+		;
+		default:
+			alert("Default message");
 		;
 	}
 
